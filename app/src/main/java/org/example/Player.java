630975dbb2ec5e5bd83e425c827b8c856062412c
bearpanda33 <sh5002871@gmail.com>
2025-07-07 16:51:55 +0900

    feat(3):Add Player class with deck and HP

---
+++ app/src/main/java/org/example/Player.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Player {
+    String name;
+    ArrayList<Monster> monsterDeck = new ArrayList<>();
+    int hp = 100;
+
+    Player(String name) {
+        this.name = name;
+        drawMonsters(8); // デッキのモンスターを8体に
+    }
+
+    public void drawMonsters(int count) {
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+            this.monsterDeck.add(new Monster(rand.nextInt(5), rand.nextInt(5)));
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Deck:" + this.name + " HP:" + this.hp + "\n");
+        for (Monster m : this.monsterDeck) {
+            sb.append(m.toString() + "\n");
+        }
+        return sb.toString();
+    }
+}
